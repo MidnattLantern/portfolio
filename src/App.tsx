52889700dab1546e6199050fb08fe0bf0be84426
Alma <isakvent@icloud.com>
2026-03-28 00:08:14 +0100

    (feature): Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,27 @@ function LandingBanner() {
     )
 }
 
+function Footer() {
+    function handleCopyDiscordUsername() {
+        navigator.clipboard.writeText("midnatt_lantern");
+    };
+
+    return (
+        <footer>
+            <p>Alma Isaksson - 2026</p>
+            <p>Email: <a href="mailto:MidnattLantern@proton.me" aria-label="developer's email address">MidnattLantern@proton.me</a></p>
+            <p>Discord username: <span role="button" tabIndex={0} className="discord-username" onClick={handleCopyDiscordUsername}>midnatt_lantern</span></p>
+            <p>GitHub: <a href="https://github.com/MidnattLantern" target="_blank">MidnattLantern</a></p>
+        </footer>
+    )
+}
+
 function App() {
 
     return (
         <>
             <LandingBanner/>
+            <Footer/>
         </>
     )
 }
