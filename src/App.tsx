a9828d4e6f351247aeb8223d2ffeb54ffe153e8e
Alma <isakvent@icloud.com>
2026-03-28 15:27:57 +0100

    (feature): Create preview gallery

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import FullStackImage from "./assets/full-stack.webp";
 import MoraGoddessSnapshotImage from "./assets/mora-goddess-snapshot.webp";
 import KorainGodSnapshotImage from "./assets/korain-god-snapshot.webp";
 import MultiCoRosterSnapshotImage from "./assets/multico-roster-snapshot.webp";
+import NicoletteImage from "./assets/nicolette.webp";
+import MoreBustImage from "./assets/mora-goddess-bust-portrait.webp";
+import FeatheredEarsImage from "./assets/feathered-ears.webp";
+import InfiniteBreadImage from "./assets/infinite-bread-avatar.webp";
+import IvyGraveyardImage from "./assets/ivy-graveyard.webp";
+import HairAndEyesImage from "./assets/hair-and-eyes.webp";
 
 function LandingBanner() {
     return (
@@ -149,6 +155,44 @@ function IllustrationPreview() {
             <div className="illustration-preview_text-alignment">
                 <p>Moving between vector design, character portrait, and web development, I have fallen where the mediums intersect: Poster art.</p>
                 <p>I am committed to remain entirely human centered in my approach to art and illustration, and do not involve artificial intelligence in any part of the creative process. Including during ideation or moodboarding.</p>
+            </div>
+            <div className="illustration-preview_art-gallery">
+                <img
+                src={NicoletteImage}
+                alt="Painting of a girl with red and clothes holding a rapier"
+                title="Nicolette"
+                loading="lazy"
+                />
+                <img
+                src={MoreBustImage}
+                alt="Painting of a goddess with ten arms and blue-pink flowing hair"
+                title="Mora Goddess Bust Portrait"
+                loading="lazy"
+                />
+                <img
+                src={FeatheredEarsImage}
+                alt="Painting of a red haired figure facing sideways, red hairs and massive black leaves covering their eyes"
+                title="Feathered Ears"
+                loading="lazy"
+                />
+                <img
+                src={InfiniteBreadImage}
+                alt="Painting of a sci-fi warrior in a leap carrying a massive sword"
+                title="Infinite bread avatar"
+                loading="lazy"
+                />
+                <img
+                src={IvyGraveyardImage}
+                alt="Painting of a red surreal landscape with a massive laying hand and a small character"
+                title="Ivy graveyard"
+                loading="lazy"
+                />
+                <img
+                src={HairAndEyesImage}
+                alt="Painting of a figure with horns and long hair covering the figure's body and surreal eyes resting on the figure's hair"
+                title="Hair and eyes"
+                loading="lazy"
+                />
             </div>
         </section>
     )
