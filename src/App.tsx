70e7f1151c9442bd9b8efa9eec1f14cf6955e9ac
Alma <isakvent@icloud.com>
2026-03-28 16:53:58 +0100

    (feature): create artstation and pinterest link cards in illustration preview

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import FeatheredEarsImage from "./assets/feathered-ears.webp";
 import InfiniteBreadImage from "./assets/infinite-bread-avatar.webp";
 import IvyGraveyardImage from "./assets/ivy-graveyard.webp";
 import HairAndEyesImage from "./assets/hair-and-eyes.webp";
+import ArtStationLogoWide from "./assets/artstation-logo-wide.svg?react";
+import PinterestLogoWide from "./assets/pinterest-logo-wide.svg?react";
 
 function LandingBanner() {
     return (
@@ -193,6 +195,17 @@ function IllustrationPreview() {
                 title="Hair and eyes"
                 loading="lazy"
                 />
+            </div>
+            <h3>Explore my art projects on my ArtStation and Pinterest pages:</h3>
+            <div className="illustration-preview_align-artstation-and-pinterest">
+                <a href="https://www.artstation.com/midnight_lantern" target="_blank" rel="noopener noreferrer" aria-label="ArtStation's logo as an external link to Alma Midnatt Lantern's ArtStation portfolio page">
+                    <ArtStationLogoWide className="illustration-preview_artstation-wide-logo"/>
+                    <span>artstation.com/midnight_lantern</span>
+                </a>
+                <a href="https://se.pinterest.com/midnatt_lantern/art-by-midnatt-lantern" target="_blank" rel="noopener noreferrer" aria-label="Pinterest's logo as an external link to Alma Midnatt Lantern's Pinterest portfolio page">
+                    <PinterestLogoWide className="illustration-preview_pinterest-wide-logo"/>
+                    <span>pinterest.com/midnatt_lantern</span>
+                </a>
             </div>
         </section>
     )
