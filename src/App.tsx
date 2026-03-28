a72f0a24413631b613e084c5cff6429eef72a9d1
Alma <isakvent@icloud.com>
2026-03-28 02:19:04 +0100

    (feature): Create a frontend preview phone layout section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { NavLink } from 'react-router';
 import './App.scss'
 import AvatarImage from "./assets/midnatt-lantern-avatar.webp";
+import FullStackImage from "./assets/full-stack.webp";
+import MoraGoddessSnapshotImage from "./assets/mora-goddess-snapshot.webp";
+import KorainGodSnapshotImage from "./assets/korain-god-snapshot.webp";
+import MultiCoRosterSnapshotImage from "./assets/multico-roster-snapshot.webp";
 
 function LandingBanner() {
     return (
@@ -46,6 +50,94 @@ function LandingNavigationSection() {
     )
 }
 
+function FrontendPreview() {
+    return (
+        <section className="frontend-preview">
+            <h2>Frontend Development</h2>
+            <div className="frontend-preview_text-alignment">
+                <p>With formal training in web development, I boast a diploma from Code Institute's Full Stack bootcamp (60 SCQF). As of today, I'm attending vocational training at Medieinstitutet to specialize in UX and frontend development.</p>
+                <p>Programming to me can be as tingly as ASMR, occupying my leisure time as well. I spend time outside work or school developing my own projects, alone and with friends, or learn new languages and frameworks. Take a look at some of my projects down below:</p>
+            </div>
+            <img
+            src={FullStackImage}
+            alt="HTML, CSS, JavaScript, TypeScript, Python, C Sharp, React, NodeJS, GitHub"
+            title="Full stack"
+            className="frontend-preview_full-stack-image"
+            loading="lazy"
+            />
+            <a
+            href="https://midnattlantern.github.io/mora-goddess"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="frontend-preview_web-project-card"
+            aria-label="Link card to the Korain God web-project"
+            >
+                <article className="frontend-preview_web-project-info">
+                    <h3>Mora Goddess</h3>
+                    <p>Commissioned work. Web art gallery featuring five illustrations.</p>
+                    <span>
+                        midnattlantern.github.io/mora-goddess
+                    </span>
+                </article>
+                <img
+                src={MoraGoddessSnapshotImage}
+                alt="Snapshot of the website Mora Goddess"
+                title="Snapshot of the website Mora Goddess"
+                className="frontend-preview_web-project-snapshot"
+                loading="lazy"
+                />
+            </a>
+            <a
+            href="https://midnattlantern.github.io/korain-god"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="frontend-preview_web-project-card frontend-preview_zag"
+            aria-label="Link card to the Korain God web-project"
+            >
+                <article className="frontend-preview_web-project-info">
+                    <h3>Korain God</h3>
+                    <p>Commissioned work. Web art gallery featuring five illustrations.</p>
+                    <span>
+                        midnattlantern.github.io/korain-god
+                    </span>
+                </article>
+                <img
+                src={KorainGodSnapshotImage}
+                alt="Snapshot of the website Korain God"
+                title="Snapshot of the website Korain God"
+                className="frontend-preview_web-project-snapshot"
+                loading="lazy"
+                />
+            </a>
+            <a
+            href="https://midnattlantern.github.io/multico-roster"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="frontend-preview_web-project-card"
+            aria-label="Link card to the MultiCo Roster web-project"
+            >
+                <article className="frontend-preview_web-project-info">
+                    <h3>MultiCo Roster</h3>
+                    <p>Volunteer/ Personal project. Hero roster in collaboration with friends.</p>
+                    <span className="newtab-icon">
+                        midnattlantern.github.io/multico-roster
+                    </span>
+                </article>
+                <img
+                src={MultiCoRosterSnapshotImage}
+                alt="Snapshot of the website MultiCo Roster"
+                title="Snapshot of the website MultiCo Roster"
+                className="frontend-preview_web-project-snapshot"
+                loading="lazy"
+                />
+            </a>
+            <NavLink to="/frontend">
+                <span>Read more</span>
+            </NavLink>
+        </section>
+    )
+}
+
 function Footer() {
     function handleCopyDiscordUsername() {
         navigator.clipboard.writeText("midnatt_lantern");
@@ -67,6 +159,7 @@ function App() {
         <>
             <LandingBanner/>
             <LandingNavigationSection/>
+            <FrontendPreview/>
             <Footer/>
         </>
     )
