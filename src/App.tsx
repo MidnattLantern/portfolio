9d8e37f3a5ec0e451a8450c0bb870c07856f8dab
Alma <isakvent@icloud.com>
2026-03-28 15:06:54 +0100

    (feature): Add description on illustration preview

--- src/App.tsx
+++ src/App.tsx
@@ -138,6 +138,22 @@ function FrontendPreview() {
     )
 }
 
+function IllustrationPreview() {
+    return (
+        <section className="illustration-preview">
+            <h2>Art & Illustration</h2>
+            <div className="illustration-preview_text-alignment">
+                <p>Fusing western cartoon and eastern anime, my visual language have remained stylized and animated for many years.</p>
+                <p>While my T-shaped skills are ranging from 3D architecture and environments, to outfit and character design, my core strength lies in fantasy and military lolita figure/ portrait art.</p>
+            </div>
+            <div className="illustration-preview_text-alignment">
+                <p>Moving between vector design, character portrait, and web development, I have fallen where the mediums intersect: Poster art.</p>
+                <p>I am committed to remain entirely human centered in my approach to art and illustration, and do not involve artificial intelligence in any part of the creative process. Including during ideation or moodboarding.</p>
+            </div>
+        </section>
+    )
+}
+
 function Footer() {
     function handleCopyDiscordUsername() {
         navigator.clipboard.writeText("midnatt_lantern");
@@ -160,6 +176,7 @@ function App() {
             <LandingBanner/>
             <LandingNavigationSection/>
             <FrontendPreview/>
+            <IllustrationPreview/>
             <Footer/>
         </>
     )
