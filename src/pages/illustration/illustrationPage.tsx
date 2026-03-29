c2acb3e626bcac72b1380abd9483198c4e8f512b
Alma <isakvent@icloud.com>
2026-03-29 20:16:07 +0200

    (feature): Add a landing banner at illustration page

--- src/pages/illustration/illustrationPage.tsx
+++ src/pages/illustration/illustrationPage.tsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react";
 import "./illustrationPage.scss";
 
+function LandingBanner () {
+    return (
+        <section className="illustration-landing-banner">
+            <h1>Art & Illustration</h1>
+        </section>
+    )
+}
+
 export function DisplayIllustrationPage() {
     useEffect(() => {
         document.title = "Midnatt Lantern | Illustration";
     }, []);
 
     return (
-        <h1>Art & Illustration</h1>
+        <LandingBanner/>
     )
 }
