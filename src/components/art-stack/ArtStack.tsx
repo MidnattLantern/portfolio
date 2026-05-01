43d5e9143463ccb633d20c687b4022f839e8bab3
Alma <isakvent@icloud.com>
2026-05-01 22:29:07 +0200

    (hotfix): Remove extension badge for illustration page

--- src/components/art-stack/ArtStack.tsx
+++ src/components/art-stack/ArtStack.tsx
@@ -3,22 +3,16 @@ import ArtStackLibrary from "./art-stack-library.json";
 
 export function ArtStack() {
     const artStackLibrary = ArtStackLibrary.map((techItem, index) => {
-        const extensionItemIcon = <div className="art-stack__extension">
-            <span>&</span>
-            <img src={techItem.extensionHref} title={techItem.extensionName} alt={techItem.extensionName} loading="lazy"/>
-        </div>
 
         const singleItemFigCaption: string = techItem.name;
-        const extendedItemFigCaption: string = `${techItem.name} & ${techItem.extensionName}`
 
         return (
             <li key={index}>
                 <figure>
                     <img src={techItem.href} title={techItem.name} alt={techItem.name} loading="lazy"/>
-                    {techItem.extensionHref && extensionItemIcon}
                     <figcaption
                     className={`${techItem.selfTaught && "art-stack__self-taught"}`}>
-                        {techItem.extensionName ? extendedItemFigCaption : singleItemFigCaption}
+                        {singleItemFigCaption}
                         {techItem.selfTaught && <span>{` (self taught)`}</span>}
                     </figcaption>
                 </figure>
